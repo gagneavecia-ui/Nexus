6be86bb42f3fffb5cc1ae19b428c0ad78e9f13ce
gagneavecia-ui <gagneavecia@gmail.com>
2026-04-22 20:49:11 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'nexus-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/profil.html',
+  '/menu.html',
+  '/investissement.html',
+  '/formations.html',
+  '/depot.html',
+  '/calendrier.html',
+  '/formation1.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache ouvert');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
